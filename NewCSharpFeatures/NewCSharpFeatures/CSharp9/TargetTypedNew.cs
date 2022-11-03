92d99ca65b2af7dc2a40b1c5a6f59437295c42e0
Samuel Gallagher <samuel.gallagher@karbonhq.com>
2022-11-03 23:26:45 +1100

    extend target-typed-new

--- NewCSharpFeatures/NewCSharpFeatures/CSharp9/TargetTypedNew.cs
+++ NewCSharpFeatures/NewCSharpFeatures/CSharp9/TargetTypedNew.cs
@@ -1,7 +1,33 @@
 ﻿namespace NewCSharpFeatures
 {
+    public class Layer1
+    {
+        public Layer2 layer { get; set; }
+
+        public Layer1(Layer2 layer)
+        {
+            this.layer = layer;
+        }
+    }
+
+    public class Layer2
+    {
+        public Layer3 layer { get; set; }
+        public Layer2(Layer3 layer)
+        {
+            this.layer = layer;
+        }
+    }
+
+    public class Layer3
+    {
+        public string Name { get; set; }
+    }
+
     internal class TargetTypedNew
     {
         List<string> strings = new();
+
+        Layer1 layers = new(new(new()));
     }
 }
