a1311a73a819ff2ac6c1d7a63e4a0f3cf1a40708
Samuel Gallagher <samuel.gallagher@karbonhq.com>
2022-11-04 01:03:49 +1100

    extend target-typed-new

--- NewCSharpFeatures/NewCSharpFeatures/CSharp10/DelegateExpressionImprovements.cs
+++ NewCSharpFeatures/NewCSharpFeatures/CSharp10/DelegateExpressionImprovements.cs
@@ -16,6 +16,9 @@ internal class DelegateExpressionImprovements
     public void funcExample()
     {
         var func = IsACriminalEnterprise;
+
+        if (func != null) return;
+        return;
     }
 
     public delegate void Action<in T1, in T2, in T3, in T4, in T5>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
